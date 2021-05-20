4054e6d3867b35e4312d8e67a6d1968616c8ef2a
Thanhnt237 <tuanthanh9a22014@gmail.com>
2021-05-20 17:24:17 +0700

    Update chiều 20.5

--- Server/server.js
+++ Server/server.js
@@ -25,6 +25,45 @@ app.set('views', './views');
 // Website routes
 app.use('/', routes);
 
+//socketIOSetup
+let listUser = [];
+io.on("connection", function(socket)
+  {
+    listUser.push(socket.id)
+    io.emit("Server-sent-userName", listUser);
+
+    console.log("Client Socket ID: " + socket.id + " connected!");
+
+    socket.on("disconnect", () => {
+      listUser.pop(socket.id);
+      io.emit("Server-sent-userName", listUser);
+      console.log("Client Socket ID: " + socket.id + " disconnected!")
+    });
+
+    socket.on("Admin-sent-Message", (data)=>{
+      if(data !== '' && data !== null){
+          io.emit("Server-reply-message", socket.id + "(admin): " + data);
+      }
+    })
+
+    socket.on("Client-sent-Message", (data)=>{
+      if(data !== '' && data !== null){
+          io.emit("Server-reply-message", socket.id + ": " + data);
+      }
+    })
+
+    socket.on("Client-is-typing", (data)=>{
+      io.emit("Server-reply-typing-event", socket.id + data);
+    })
+
+    socket.on("message", data =>
+  {
+    //sau khi lắng nghe dữ liệu, server phát lại dữ liệu này đến các client khác
+            io.emit("Server-sent-data", data);
+  });
+  });
+
+
 server.listen(PORT, () => {
   console.log("Server running on PORT -> " + PORT);
 });
--- UI/src/app/Pages/chat/chat.component.ts
+++ UI/src/app/Pages/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from "../../Services/websocket.service";
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  title = 'app';
 
-  constructor() { }
+  showSomething: any[] = [];
+  listUser: any[] = [];
+  listMessages: any[] = [];
+  messageFromClient: any;
+
+  constructor(private websocketService: WebsocketService) { }
 
   ngOnInit(): void {
-  }
+    this.websocketService.listen("Server-reply-message").subscribe((data:any)=>{
+  this.listMessages.push(data);
+  this.messageFromClient = '';
+  console.log(this.listMessages);
+})
+
+this.websocketService.listen("Server-sent-userName").subscribe((data:any)=>{
+this.listUser = data;
+console.log(this.listUser);
+})
+
+this.websocketService.listen("Server-sent-data").subscribe((data:any)=>{
+this.showSomething.push(data);
+console.log(this.showSomething);
+})
+}
+
+SendMessage(data: any){
+this.websocketService.emit("Client-sent-Message", data);
+}
+
+TypingEvent(){
+this.websocketService.emit("Client-is-typing", " is typing...")
+}
 
 }
